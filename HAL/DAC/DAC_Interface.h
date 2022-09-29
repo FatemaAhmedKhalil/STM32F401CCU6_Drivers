90a84c6487a1260138d16ea3c0edfef753bd19d4
Fatema Ahmed <100099549+FatemaAhmedKhalil@users.noreply.github.com>
2022-09-29 02:19:38 +0200

    Add files via upload

---
+++ HAL/DAC/DAC_Interface.h
@@ -0,0 +1,42 @@
+﻿/***********************************************************************************************/
+/***********************************************************************************************/
+/************************************* Author: Fatema Ahmed ************************************/
+/***************************************** Layer: MCAL *****************************************/
+/******************************************* SWC: DAC ******************************************/
+/***************************************** Version: 1.0 ****************************************/
+/***********************************************************************************************/
+/***********************************************************************************************/
+
+#ifndef DAC_Interface
+#define DAC_Interface
+
+/*********************************/
+/* Define						 */
+/*		1) DAC Port				 */
+/*		2) DAC Bits' Pins		 */
+/*********************************/
+
+#define DAC_GPIOA	1
+#define DAC_GPIOB	2
+#define DAC_GPIOC	3
+
+#define DAC_PIN0	0
+#define DAC_PIN1	1
+#define DAC_PIN2	2
+#define DAC_PIN3	3
+#define DAC_PIN4	4
+#define DAC_PIN5	5
+#define DAC_PIN6	6
+#define DAC_PIN7	7
+#define DAC_PIN8	8
+#define DAC_PIN9	9
+#define DAC_PIN10	10
+#define DAC_PIN11	11
+#define DAC_PIN12	12
+#define DAC_PIN13	13
+#define DAC_PIN14	14
+#define DAC_PIN15	15
+
+void DAC_voidPlaySong (u8 u8Song[], u32 u32SongLength, u8 u8PortDAC, u8 u8Pin0DAC, u8 u8Pin1DAC, u8 u8Pin2DAC, u8 u8Pin3DAC, u8 u8Pin4DAC, u8 u8Pin5DAC, u8 u8Pin6DAC, u8 u8Pin7DAC, u32 u32SongTickTime);
+
+#endif
---
+++ HAL/DAC/DAC_Program.c
@@ -0,0 +1,71 @@
+/***********************************************************************************************/
+/***********************************************************************************************/
+/************************************* Author: Fatema Ahmed ************************************/
+/***************************************** Layer: MCAL *****************************************/
+/******************************************* SWC: DAC ******************************************/
+/***************************************** Version: 1.0 ****************************************/
+/***********************************************************************************************/
+/***********************************************************************************************/
+
+#include "STD_Types.h"
+#include "BitOperations.h"
+
+
+#include "DAC_Interface.h"
+#include "DAC_Private.h"
+
+#include "GPIO_Interface.h"
+#include "STK_Interface.h"
+
+u8 Global_DAC_GPIO;
+
+u8 Global_DAC_Bit0;
+u8 Global_DAC_Bit1;
+u8 Global_DAC_Bit2;
+u8 Global_DAC_Bit3;
+u8 Global_DAC_Bit4;
+u8 Global_DAC_Bit5;
+u8 Global_DAC_Bit6;
+u8 Global_DAC_Bit7;
+
+u8*Global_SongPointer;
+u32 Global_SongLength;
+
+void DAC_voidPlaySong (u8 u8Song[], u32 u32SongLength, u8 u8PortDAC, u8 u8Pin0DAC, u8 u8Pin1DAC, u8 u8Pin2DAC, u8 u8Pin3DAC, u8 u8Pin4DAC, u8 u8Pin5DAC, u8 u8Pin6DAC, u8 u8Pin7DAC, u32 u32SongTickTime)
+{
+	Global_SongPointer = u8Song;
+	Global_SongLength = u32SongLength;
+	
+	Global_DAC_GPIO = u8PortDAC;
+	
+	Global_DAC_Bit0 = u8Pin0DAC;
+	Global_DAC_Bit1 = u8Pin1DAC;
+	Global_DAC_Bit2 = u8Pin2DAC;
+	Global_DAC_Bit3 = u8Pin3DAC;
+	Global_DAC_Bit4 = u8Pin4DAC;
+	Global_DAC_Bit5 = u8Pin5DAC;
+	Global_DAC_Bit6 = u8Pin6DAC;
+	Global_DAC_Bit7 = u8Pin7DAC;
+	
+	STK_voidSetInterval_Periodic (u32SongTickTime, DAC_ISR);
+}
+
+/** DAC ISR **/
+void DAC_ISR(void)
+{
+	static u32 SongIterator = 0;
+	
+	GPIO_u8FastControlPinValue (Global_DAC_GPIO, Global_DAC_Bit0, GET_BIT(Global_SongPointer[SongIterator],0) );
+	GPIO_u8FastControlPinValue (Global_DAC_GPIO, Global_DAC_Bit1, GET_BIT(Global_SongPointer[SongIterator],1) );
+	GPIO_u8FastControlPinValue (Global_DAC_GPIO, Global_DAC_Bit2, GET_BIT(Global_SongPointer[SongIterator],2) );
+	GPIO_u8FastControlPinValue (Global_DAC_GPIO, Global_DAC_Bit3, GET_BIT(Global_SongPointer[SongIterator],3) );
+	GPIO_u8FastControlPinValue (Global_DAC_GPIO, Global_DAC_Bit4, GET_BIT(Global_SongPointer[SongIterator],4) );
+	GPIO_u8FastControlPinValue (Global_DAC_GPIO, Global_DAC_Bit5, GET_BIT(Global_SongPointer[SongIterator],5) );
+	GPIO_u8FastControlPinValue (Global_DAC_GPIO, Global_DAC_Bit6, GET_BIT(Global_SongPointer[SongIterator],6) );
+	GPIO_u8FastControlPinValue (Global_DAC_GPIO, Global_DAC_Bit7, GET_BIT(Global_SongPointer[SongIterator],7) );
+
+	if ( SongIterator < Global_SongLength )
+		SongIterator++;
+	else
+		SongIterator=0; // Replay the Song
+}
